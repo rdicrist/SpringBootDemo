826dd72076bd2a61692e1fcc2db72d42237bdf2a
Ricki DiCristoforo <rdicrist@gmail.com>
2024-06-03 15:51:59 -0700

    first commit

---
+++ src/main/java/com/example/Controllers/GreetingController.java
@@ -0,0 +1,32 @@
+package com.example.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+//    @GetMapping("/")
+//    public ResponseEntity<String> hello() {
+//        return new ResponseEntity<>("Hello!", HttpStatus.OK);
+//    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello!";
+    }
+
+//    @GetMapping("/name")
+//    public ResponseEntity<String> helloName(@RequestParam("name") String name) {
+//        return new ResponseEntity<>("Hello "+name+"!", HttpStatus.OK);
+//    }
+
+    @GetMapping("/name")
+    public String helloName(@RequestParam("name") String name) {
+        return "Hello "+name+"!";
+    }
+}
---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application running...");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
